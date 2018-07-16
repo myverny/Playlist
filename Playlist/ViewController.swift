87e71b75259c8be7c738f4fd6eb2dec812cc5442
dev <dev@youngjae-macbook-pro.local>
2018-07-16 13:37:13 +0900

    Initial Commit

---
+++ Playlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Playlist
+//
+//  Created by dev on 2018. 7. 16..
+//  Copyright © 2018년 dorenza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
