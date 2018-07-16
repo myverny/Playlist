a7c9a068c0aae8b43287f79e69b7d3d96fe318dc
myverny <youngjae.choi@dorenza.co>
2018-07-16 14:24:36 +0900

    add navi/tab view add HomeViewController class

---
+++ Playlist/View/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  Playlist
+//
+//  Created by dev on 2018. 7. 16..
+//  Copyright © 2018년 dorenza. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
