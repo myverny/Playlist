4ad3a45cf02418576e59f97c665ada50ba7b5360
gobright <gobright12@gmail.com>
2018-07-19 15:46:43 +0900

    added playlistView

---
+++ Playlist/View/Common/PlaylistViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PlaylistViewController.swift
+//  Playlist
+//
+//  Created by DONGHYUN KIM on 2018. 7. 19..
+//  Copyright © 2018년 dorenza. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
