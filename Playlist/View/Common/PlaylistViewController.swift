a55b5061be6f1e6d7caf1e4efa0ac0279b8fe1b6
gobright <gobright12@gmail.com>
2018-07-19 16:52:39 +0900

    temp commit

--- Playlist/View/Common/PlaylistViewController.swift
+++ Playlist/View/Common/PlaylistViewController.swift
@@ -8,9 +8,28 @@
 
 import UIKit
 
-class PlaylistViewController: UIViewController {
+class PlaylistViewController: UIViewController, UITableViewDataSource {
     
     var videos: [String]!
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        return cell
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
